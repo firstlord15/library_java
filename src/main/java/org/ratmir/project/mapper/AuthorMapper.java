1e926f2eff75bd8565f6e28304ec082a7ab2f5c2
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-04-22 01:02:49 +0300

    Create: Mapper for author

---
+++ src/main/java/org/ratmir/project/mapper/AuthorMapper.java
@@ -0,0 +1,24 @@
+package org.ratmir.project.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.ratmir.project.dto.author.AuthorDetailDTO;
+import org.ratmir.project.dto.author.AuthorShortDTO;
+import org.ratmir.project.dto.author.CreateAuthorDTO;
+import org.ratmir.project.dto.author.UpdateAuthorDTO;
+import org.ratmir.project.models.Author;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface AuthorMapper {
+    AuthorDetailDTO toDTO(Author author);
+
+    AuthorShortDTO toShortDTO(Author author);
+
+    @Mapping(target = "id", ignore = true)
+    Author fromCreatedDTO(CreateAuthorDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateFromDTO(UpdateAuthorDTO dto, @MappingTarget Author author);
+}
