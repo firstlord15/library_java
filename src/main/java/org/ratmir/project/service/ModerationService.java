ec2ff52652fa20392d815910bbe12abe9bcf4a2d
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-04-23 22:15:02 +0300

    Update: Add a method to simplify (moderateBook) and update logs

--- src/main/java/org/ratmir/project/service/ModerationService.java
+++ src/main/java/org/ratmir/project/service/ModerationService.java
@@ -3,8 +3,12 @@ package org.ratmir.project.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ratmir.project.dto.book.BookDetailDTO;
+import org.ratmir.project.dto.moderation.ModerationRecordDTO;
 import org.ratmir.project.enums.ModerationStatus;
 import org.ratmir.project.exception.ResourceNotFoundException;
+import org.ratmir.project.mapper.BookMapper;
+import org.ratmir.project.mapper.ModerationMapper;
 import org.ratmir.project.models.Book;
 import org.ratmir.project.models.ModerationRecord;
 import org.ratmir.project.models.User;
@@ -21,69 +25,72 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class ModerationService {
+    private final BookMapper bookMapper;
+    private final ModerationMapper mapper;
     private final BookRepository bookRepository;
     private final UserRepository userRepository;
     private final ModerationRepository repository;
 
     @Transactional
-    public ModerationRecord approveBook(UUID moderatorId, UUID bookId, String comment) {
-        Book book = getBookOrThrow(bookId);
-        User moderator = getModeratorOrThrow(moderatorId);
-
-        book.setStatus(ModerationStatus.APPROVED);
-        bookRepository.save(book);
-
-        ModerationRecord moderationRecord = new ModerationRecord();
-        moderationRecord.setBook(book);
-        moderationRecord.setModerator(moderator);
-        moderationRecord.setComment(comment);
-        moderationRecord.setStatus(ModerationStatus.APPROVED);
-        moderationRecord.setCreatedAt(LocalDateTime.now());
-
-        log.info("APPROVE Book: {} by moderator: {}", bookId, moderatorId);
-        return repository.save(moderationRecord);
+    public ModerationRecordDTO approveBook(UUID moderatorId, UUID bookId, String comment) {
+        return moderateBook(moderatorId, bookId, comment, ModerationStatus.APPROVED);
     }
 
     @Transactional
-    public ModerationRecord rejectBook(UUID moderatorId, UUID bookId, String comment) {
+    public ModerationRecordDTO rejectBook(UUID moderatorId, UUID bookId, String comment) {
+        return moderateBook(moderatorId, bookId, comment, ModerationStatus.REJECTED);
+    }
+
+    public ModerationRecordDTO moderateBook(UUID moderatorId, UUID bookId, String comment, ModerationStatus status) {
         Book book = getBookOrThrow(bookId);
         User moderator = getModeratorOrThrow(moderatorId);
 
-        book.setStatus(ModerationStatus.REJECTED);
+        book.setStatus(status);
         bookRepository.save(book);
 
         ModerationRecord moderationRecord = new ModerationRecord();
         moderationRecord.setBook(book);
         moderationRecord.setModerator(moderator);
         moderationRecord.setComment(comment);
-        moderationRecord.setStatus(ModerationStatus.REJECTED);
-        moderationRecord.setCreatedAt(LocalDateTime.now());
+        moderationRecord.setStatus(status);
 
-        log.info("REJECT Book: {} by moderator: {}", bookId, moderatorId);
-        return repository.save(moderationRecord);
+        log.info("{} book: {} by moderator: {}", status, bookId, moderatorId);
+        ModerationRecord moderation = repository.save(moderationRecord);
+        return mapper.toDTO(moderation);
     }
 
-    public List<ModerationRecord> getHistoryByBook(UUID bookId) {
-        log.info("Get History By Book: {}", bookId);
-        return repository.findByBookId(bookId);
+    public List<ModerationRecordDTO> getHistoryByBook(UUID bookId) {
+        log.debug("Fetching history by bookId: {}", bookId);
+        return repository.findByBookId(bookId)
+                .stream()
+                .map(mapper::toDTO)
+                .toList();
     }
 
-    public List<ModerationRecord> getHistoryByModerator(UUID moderatorId) {
-        log.info("Get History By Moderator: {}", moderatorId);
-        return repository.findByModeratorId(moderatorId);
+    public List<ModerationRecordDTO> getHistoryByModerator(UUID moderatorId) {
+        log.debug("Fetching history by moderatorId: {}", moderatorId);
+        return repository.findByModeratorId(moderatorId)
+                .stream()
+                .map(mapper::toDTO)
+                .toList();
     }
 
-    public ModerationRecord getLastRecordByBook(UUID bookId) {
-        log.info("Get Last Record By Book: {}", bookId);
-        return repository.findTopByBookIdOrderByCreatedAtDesc(bookId)
+    public ModerationRecordDTO getLastRecordByBook(UUID bookId) {
+        log.debug("Fetching last record by bookId: {}", bookId);
+        ModerationRecord moderation = repository.findTopByBookIdOrderByCreatedAtDesc(bookId)
                 .orElseThrow(() -> new ResourceNotFoundException("No moderation records for book: " + bookId));
+        return mapper.toDTO(moderation);
     }
 
-    public List<Book> getPendingBooks() {
-        log.info("Get Pending Books");
-        return bookRepository.findByStatus(ModerationStatus.PENDING);
+    public List<BookDetailDTO> getPendingBooks() {
+        log.info("Fetching pending books");
+        return bookRepository.findByStatus(ModerationStatus.PENDING)
+                .stream()
+                .map(bookMapper::toBookDetailDTO)
+                .toList();
     }
 
+    // Вспомогательные методы
     private Book getBookOrThrow(UUID bookId) {
         return bookRepository.findById(bookId)
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found: " + bookId));
