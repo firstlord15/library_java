597a0e1420f9af2f820441be4231a682e29552df
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-04-23 22:11:56 +0300

    Update: Add findByName method for genre repository

--- src/main/java/org/ratmir/project/repository/GenreRepository.java
+++ src/main/java/org/ratmir/project/repository/GenreRepository.java
@@ -4,9 +4,10 @@ import org.ratmir.project.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, UUID> {
-
+    Optional<Genre> findByName(String name);
 }
