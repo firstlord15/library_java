733283c729d47f3f95ef7b01f03b18539c93cc01
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-05-15 01:28:24 +0300

    fix: deleted useless import ModerationRepository

--- src/main/java/org/ratmir/project/repository/ModerationRepository.java
+++ src/main/java/org/ratmir/project/repository/ModerationRepository.java
@@ -1,6 +1,5 @@
 package org.ratmir.project.repository;
 
-import org.ratmir.project.enums.ModerationStatus;
 import org.ratmir.project.models.ModerationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
